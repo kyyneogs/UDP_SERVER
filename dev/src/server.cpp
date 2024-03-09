3eb5feedca527b358003b8f653a89c28357e7f72
kyyneogs <oks990304@gmail.com>
2024-03-09 15:54:19 +0900

    feat: Add server class

---
+++ dev/src/server.cpp
@@ -0,0 +1,61 @@
+#include "../include/server.hpp"
+
+UdpServer::UdpServer() {
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    memset(&clientAddr, 0, sizeof(clientAddr));
+}
+
+UdpServer::~UdpServer() {
+    close(serverSock);
+}
+
+void UdpServer::setPort(int port) {
+    this->port = port;
+}
+
+int UdpServer::sockInit() {
+    serverSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (serverSock == -1) {
+        // error_handling("udp socket create failed.");
+        return 1;
+    }
+    
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(port);
+
+    if (bind(serverSock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+        // error_handling("udp socket bind failed.");
+        return 2;
+    }
+
+    return 0;
+}
+
+void UdpServer::recvMessage() {
+    char buff[1024];
+    socklen_t recvLen;
+
+    recvLen = sizeof(clientAddr);
+    messageSize = recvfrom(serverSock, buff, sizeof(buff), 0,
+                    (struct sockaddr*)&clientAddr, &recvLen);
+
+    if (messageSize == -1){
+        std::cout << "recive Failed.";
+    }
+
+    switch(messageSize){
+        case -1:
+            std::cout << "recieve Failed.";
+            close(serverSock);
+            exit(1);
+            break;
+        
+        case 0:
+            break;
+        
+        default:
+            buff[messageSize] = '\0';
+            std::cout << buff << "\n";
+    }
+}
